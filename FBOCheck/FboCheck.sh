a0ede4881475382b84c17d383e7d8e78b8cff4df
Prslc <nicolmarylyn11761252@gmail.com>
2024-04-30 08:39:00 +0800

    add fbo/system/etc/init/path if

--- FBOCheck/FboCheck.sh
+++ FBOCheck/FboCheck.sh
@@ -2,21 +2,27 @@ if [ "$(getprop init.svc.fbo-installd)" = "running" ]; then
     echo init_FBO正在运行😋
 else
     echo init_FBO已停止😭
+    echo 请查看init.svc.fbo-installd 是否为running
+    echo
 fi
 if [ "$(getprop persist.sys.fboservice.ctrl)" = "true" ]; then
     echo FBO已启动😋
 else
     echo FBO未开启😭
+    echo 请查看persist.sys.fboservice.ctrl是否为true
+    echo
 fi
 
 if [ "$(getprop persist.sys.stability.miui_fbo_enable)" = "true" ]; then
     echo MIUI_FBO已启动😋
 else
     echo MIUI_FBO未开启😭
+    echo 请查看persist.sys.stability.miui_fbo_enable是否为true
+    echo
 fi
 echo
-if [ -f "/system_ext/etc/init/memory.fbo.native@1.0-service.rc" ]; then
-    echo rc文件存在🥰
+if [ -f "/system_ext/etc/init/memory.fbo.native@1.0-service.rc" ]||［ -f "/system/etc/init/memory.fbo.native@1.0-service.rc"］; then
+    echo fbo.native.rc存在😋
 else 
     echo rc文件不存在😭
     echo 请查看目录/system_ext/etc/init/init确认memory.fbo.native@1.0-service.rc是否存在
@@ -27,5 +33,5 @@ if [ -f "/system_ext/bin/FboNativeService" ]; then
     echo FboNativeService存在😋
 else 
     echo FboNativeService不存在😭
-    echo 请查看/system_ext/bin/下是否存在FboNativeService
+    echo 请查看目录/system_ext/bin/是否存在FboNativeService
 fi
