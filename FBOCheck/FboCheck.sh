b05c4886d8d3b533431cbd9824741fdd840a7af8
Prslc <nicolmarylyn11761252@gmail.com>
2024-04-30 08:26:39 +0800

    Update FboCheck.sh

--- FBOCheck/FboCheck.sh
+++ FBOCheck/FboCheck.sh
@@ -1,3 +1,8 @@
+if [ "$(getprop init.svc.fbo-installd)" = "running" ]; then
+    echo init_FBO正在运行😋
+else
+    echo init_FBO已停止😭
+fi
 if [ "$(getprop persist.sys.fboservice.ctrl)" = "true" ]; then
     echo FBO已启动😋
 else
@@ -5,8 +10,22 @@ else
 fi
 
 if [ "$(getprop persist.sys.stability.miui_fbo_enable)" = "true" ]; then
-    echo MIUI FBO已启动😋
+    echo MIUI_FBO已启动😋
 else
-    echo MIUI FBO未开启😭
+    echo MIUI_FBO未开启😭
 fi
-#setprop persist.sys.fboservice.ctrl true
+echo
+if [ -f "/system_ext/etc/init/memory.fbo.native@1.0-service.rc" ]; then
+    echo rc文件存在🥰
+else 
+    echo rc文件不存在😭
+    echo 请查看目录/system_ext/etc/init/init确认memory.fbo.native@1.0-service.rc是否存在
+    echo
+fi
+
+if [ -f "/system_ext/bin/FboNativeService" ]; then
+    echo FboNativeService存在😋
+else 
+    echo FboNativeService不存在😭
+    echo 请查看/system_ext/bin/下是否存在FboNativeService
+fi
