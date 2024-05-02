fcee8528d4e6700876bc401a9a45bada6d2d9cbf
prslc <nicolmarylyn11761252@gmail.com>
2024-05-02 21:24:05 +0800

    fix fileNoactive bug

--- Device/Device.sh
+++ Device/Device.sh
@@ -1,8 +1,15 @@
-#查看内核
-echo "手机型号：$(getprop ro.product.odm.marketname) ($(getprop ro.product.board))"
-echo "安卓版本：$(getprop ro.build.version.release)"
-echo "安全补丁：$(getprop ro.build.version.security_patch)"
-echo "固件版本：$(getprop persist.sys.grant_version)"
+if getprop ro.product.odm.marketname >/dev/null 2>&1; then
+    echo "手机型号：$(getprop ro.product.odm.marketname) ($(getprop ro.product.board))"
+fi
+if getprop ro.build.version.release >/dev/null 2>&1; then
+    echo "安卓版本：$(getprop ro.build.version.release)"
+fi
+if getprop ro.build.version.security_patch >/dev/null 2>&1; then
+    echo "安全补丁：$(getprop ro.build.version.security_patch)"
+fi
+if getprop persist.sys.grant_version >/dev/null 2>&1; then
+    echo "固件版本：$(getprop persist.sys.grant_version)"
+fi
 echo "内核版本：$(uname -r)"
 compile_time=$(uname -v)
 datetime_part=$(echo "$compile_time" | awk '{print $6, $7, $8, $9, $10}')
@@ -33,13 +40,37 @@ case $(echo "$compile_time" | awk '{print $4}') in
 esac
 time_part=$(echo "$compile_time" | awk '{print $7}')
 echo "编译时间：$(echo "$compile_time" | awk '{print $9}')年$chinese_month$(echo "$compile_time" | awk '{print $6}')日 $chinese_day $time_part"
-echo "内核信息: $(awk -F ' ' '{print $5,$7,$8}' /proc/version | tr -d '()' | tr -d ',')\n"
-soc=$(getprop ro.soc.model)
-echo "处理器：$soc"
+echo "内核信息: $(awk -F ' ' '{print $5,$7,$8}' /proc/version | tr -d '()' | tr -d ',')"
+if getprop getprop ro.soc.model >/dev/null 2>&1; then
+    echo "处理器：$(getprop ro.soc.model)"
+fi
 zip=$(cat /sys/block/zram0/comp_algorithm | cut -d '[' -f2 | cut -d ']' -f1)
-echo "ZRAM："$(awk 'NR > 1 {size=$3/(1024*1024); printf "%.1fG\n", size}' /proc/swaps) "($zip)"
+echo "ZRAM大小："$(awk 'NR > 1 {size=$3/(1024*1024); printf "%.1fG\n", size}' /proc/swaps) "($zip)"
 
 echo ""
+
+#查看电池 传播者：Rock&Z
+if [ -f "/sys/class/power_supply/battery/charge_full_design" ]; then
+    charge_full_design=$(su -c cat /sys/class/power_supply/battery/charge_full_design)
+    echo "电池设计容量：$(echo "scale=0;$charge_full_design/1000"|bc)mAh"
+fi
+
+if [ -f "/sys/class/power_supply/battery/cycle_count" ]; then
+    cycle_count=$(su -c cat /sys/class/power_supply/battery/cycle_count)
+    echo "循环次数：$cycle_count次"
+fi
+
+if [ -f "/sys/class/power_supply/battery/charge_full" ]; then
+    charge_full=$(su -c cat /sys/class/power_supply/battery/charge_full)
+    echo "电池当前充满：$(echo "scale=0;$charge_full/1000"|bc)mAh"
+fi
+
+if [ -f "/sys/class/power_supply/battery/charge_full" ] && [ -f "/sys/class/power_supply/battery/charge_full_design" ]; then
+    JKD=$(echo "100*$charge_full/$charge_full_design"|bc)
+    echo "当前电池健康度：$JKD%"
+fi
+echo " "
+
 if env | grep -qn 'ksu'; 
 then
     echo "Root环境：KernelSU"
@@ -55,11 +86,11 @@ echo " "
 # 查看冻结源码 作者：JARK006
 if [ -f "/data/system/NoActive/log" ]; then
     Filever=$(head -n 1 /data/system/NoActive/log | awk '{print $NF}')
+    echo "墓碑环境：Noactive($Filever)"
 fi
 Lspver=$(grep -l "modules" /data/adb/lspd/log/* | xargs sed -n '/当前版本/s/.*当前版本 \([0-9]*\).*/\1/p')
-
-if pm list packages | grep -qw "cn.myflv.noactive"; then
-    echo "墓碑环境：Noactive($Lspver)"
+if [ ! -z "$Lspver" ]; then
+echo "墓碑环境：Noactive($Lspver)"
 fi
 apk=$(dumpsys package com.sidesand.millet | grep versionName | awk -F' ' '{print $1}' | cut -d '=' -f2)
 if pm list packages | grep -qw "com.sidesand.millet1"; then
