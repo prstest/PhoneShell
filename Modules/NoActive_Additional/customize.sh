5d7fb73d11bf99f2cb78b50c6f7c76b4f4b46055
prslc <nicolmarylyn11761252@gmail.com>
2024-08-01 18:49:45 +0800

    add Magisk Check

--- Modules/NoActive_Additional/customize.sh
+++ Modules/NoActive_Additional/customize.sh
@@ -1,1 +1,6 @@
 #!/system/bin/sh
+if [ $MAGISK_VER_CODE -eq 27005] && [-z $KSU ] && [ -z $APATCH ]; then
+    ui_print "- 检测到Magisk版本为27005，已停止安装"
+    ui_print "- 请更新Magisk之后重新安装该模块"
+    abort "Stop installation Module"
+fi
