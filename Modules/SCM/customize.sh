04382a87c94b0e768a72ceb0dc3207b4008a010c
prslc <nicolmarylyn11761252@gmail.com>
2025-01-08 22:09:46 +0800

    Block managers that cannot use -w

--- Modules/SCM/customize.sh
+++ Modules/SCM/customize.sh
@@ -1,4 +1,21 @@
 #!/system/bin/sh
-MODDIR=${0%/*}
+# 拦截 Magisk 为 27005 的版本
+if [[ "$MAGISK_VER_CODE" -eq 27005 && -z "$KSU" && -z "$APATCH" ]]; then
+    ui_print "- 检测到 Magisk 版本为 27005，已停止安装"
+    ui_print "- 请更新 Magisk 之后重新安装该模块"
+    abort "Stop installation Module"
+fi
+
+# 检测 Magisk 版本是否小于 27000
+if [[ "$MAGISK_VER_CODE" -lt 27000 && -z "$KSU" && -z "$APATCH" ]]; then
+    ui_print "你的 Magisk 版本 < 27000 已被拦截"
+    abort "Stop installation Module"
+fi
+
+# 检测 KernelSU 版本是否小于 11422
+if [[ "$KSU" == "true" && "$KSU_VER_CODE" -lt 11422 ]]; then
+    ui_print "你的 KernelSU 管理器版本 < 11422 已被拦截"
+    abort "Stop installation Module"
+fi
 
 ui_print "你已经成功安装SCM (Smart Charge Manager)"
